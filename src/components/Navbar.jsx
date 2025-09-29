afc1a69c4430a67193a9dbd4dfe3805ac329642e
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-09-29 22:58:08 +0600

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
+import { MapPin } from 'lucide-react'
 import React from 'react'
+import { FaCaretDown } from 'react-icons/fa'
+import { Link } from 'react-router'
 
 const Navbar = () => {
+  const location = false
   return (
-    <div>
-        Navvar
+    <div className=' bg-white py-3 shadow-2xl'>
+        <div className='max-w-6xl mx-auto flex justify-between items-center'>
+          {/* logo section */}
+          <div className='flex gap-7 items-center'>
+            <Link to={'/'}> <h1 className='font-bold text-3xl'> <span className='text-red-500 font-serif'>N</span>ahar Shop</h1> </Link>
+            <div className='flex gap-1 cursor-pointer text-gray-700 items-center'>
+              <MapPin className='text-red-500' />
+              <span className='font-semibold'>{location ? <div></div>:"Add Address" } </span>
+              <FaCaretDown/>
+
+            </div>
+          </div>
+          {/* menu section */}
+          <nav>
+            <ul className='flex gap-7 items-center text-xl font-semibold'>
+              <li>Home</li>
+              <li>Products</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
     </div>
   )
 }
