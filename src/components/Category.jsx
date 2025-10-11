61265127188c685cdd12c302583753291188f78b
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-10-11 22:47:12 +0600

    category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import {  getData } from '../context/DataContext'
 //useContext
 const Category = () => {
-    const {data} = getData
+    const {data} = getData()
+
+    const getUniqueCategory = (data, property) => {
+      let newVA1 = data?.map((curElem) =>{
+        return curElem[property]
+      })
+    }
+    
   return (
     <div>Category</div>
   )
