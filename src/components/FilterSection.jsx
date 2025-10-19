3d95014cce0b8e1f6dc0ad8a2953db5800dd5dad
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-10-19 20:31:39 +0600

    prodcust item

---
+++ src/components/FilterSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FilterSection = () => {
+  return (
+    <div>
+        FilterSection
+
+    </div>
+  )
+}
+
+export default FilterSection
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { getData } from '../context/DataContext'
+import FilterSection from '../components/FilterSection'
+import Loading from "../assets/Loading4.webm"
+import ProductCard from '../components/ProductCard'
+
+
 
 
 const Products = () => {
+  const {data, fetchAllProducts } = getData()
+  useEffect(()=>{
+    fetchAllProducts()
+  },[])
+  
   
 
   return (
     <div>
       <div className='max-w-6xl mx-auto px-4 mb-10'>
         {
-          
+          data?.length > 0 ? (
+            <div className='flex gap-8'>
+              <FilterSection/>
+              <div>
+                {
+                  data?.map((Products,index)=> {
+                    return <ProductCard key={index} />
+                  })
+
+                }
+              </div>
+
+            </div>
+          ): (
+            <div className='flex items-center justify-center h-[500px] '>
+              <video muted autoPlay loop >
+                <source src={Loading} type='video/webm'/>
+              </video>
+
+            </div>
+          )
         }
       </div>
     </div>
