a5984f2d26b243c6963f70ae3ee7a62ec220f503
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-10-21 04:55:57 +0600

    screenshort

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = ({product}) => {
   return (
     <div className='border relative border-gray-100 rounded-2xl cursor-pointer hover:scale-102 hover:shadow-2xl hover:p-10 transition-all p-5 h-max'>
 
-      <img src={product.image} alt=""  />
+      <img src={product.image} alt="" className='bg-gray-100 rounded-md ' />
       <h1 className='line-clamp-2 p-5 font-semibold'> {product.title} </h1>
       <p className='my-2 text-lg text-gray-800 font-bold'> ${product.price} </p>
       <button className='bg-red-500 px-3 py-2 text-lg rounded-md text-white w-full cursor-pointer flex gap-12 items-center justify-center font-semibold  '> <IoCartOutline className='w-6 h-6' />  Add to Cart </button>
