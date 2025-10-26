258b5007c26c2605a36dfa453a4461faa1815dea
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-10-26 23:03:52 +0600

    pages add

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -4,6 +4,7 @@ import { getData } from '../context/DataContext'
 const FilterSection = ({search, setSearch, category,setCategory, brand, setBrand, priceRange, setPriceRange,handleCategoryChange, handleBrandChange }) => {
   const { categoryOnlyData,brandOnlyData} = getData()
   return (
+    //search user input
     <div className='bg-gray-100 mt-10 p-4 rounded-md h-max '>
         <input type="text" placeholder='search..'
         value={search}
@@ -40,11 +41,11 @@ const FilterSection = ({search, setSearch, category,setCategory, brand, setBrand
         <h1 className='mt-5 font-semibold text-xl mb-3'> Price Range </h1>
         <div>
           <label htmlFor=""> Price Range: ${priceRange[0]} - ${priceRange[1]} </label>
-          <input type="range" name='' id='' value={priceRange[1]} onChange={(e)=>setPriceRange([priceRange[0],Number(e.target.value )])}/>
+          <input type="range" min='0' max='200' name='' id='' value={priceRange[1]} onChange={(e)=>setPriceRange([priceRange[0],Number(e.target.value )])}/>
           
         </div>
         
-        <button className='bg-red-500 text-white rounded-md px-3 py-1 mt-5 cursor-pointer' > Reset Filters </button>
+        <button className='bg-red-500 text-white rounded-md px-3 py-1 mt-5 cursor-pointer'onClick={()=>{setSearch('');setCategory('All'); setBrand('All'); setPriceRange([0,200])} } > Reset Filters </button>
 
 
     </div>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,7 @@ import { getData } from '../context/DataContext'
 import FilterSection from '../components/FilterSection'
 import Loading from "../assets/Loading4.webm"
 import ProductCard from '../components/ProductCard'
+import Pagination from '../components/Pagination'
 
 
 
@@ -12,7 +13,8 @@ const Products = () => {
   const [search, setSearch] = useState("")
   const [category, setCategory] = useState("All")
   const [brand, setBrand] = useState("All")
-  const [priceRange, setPriceRange] = useState([0,5000])
+  const [priceRange, setPriceRange] = useState([0,200])
+  const [page, setPage] = useState(1)
   
   useEffect(()=>{
     fetchAllProducts()
@@ -24,13 +26,19 @@ const Products = () => {
   }
   const handleBrandChange =(e)=> {
     setBrand(e.target.value)
-    
   }
+  const pageHandler = (selectedPage)=> {
+    setPage(selectedPage)
+  }
+
+  
 
    const filteredData = data?.filter((item)=> item.title.toLowerCase().includes(search.toLowerCase()) && (category === "All" || item.category === category) &&
     (brand === "All" || item.brand === brand) &&
      item.price >= priceRange[0] && 
      item.price <= priceRange[1])
+
+     const dynamicPage = Math.ceil(filteredData?.length / 6)
   
 
   return (
@@ -38,21 +46,24 @@ const Products = () => {
       <div className='max-w-6xl mx-auto px-4 mb-10'>
         {
           data?.length > 0 ? (
+            <>
+            
             <div className='flex gap-8'>
               <FilterSection search={search} setSearch ={setSearch} category={category} setCategory={setCategory} brand={brand} setBrand={setBrand} priceRange={priceRange} setPriceRange={setPriceRange} handleCategoryChange={handleCategoryChange} handleBrandChange={handleBrandChange}/>
 
               <div className='grid grid-cols-3 gap-5 mt-10'>
                 {/* all data get from props product={product} */}
                 {
-                  filteredData?.map((product, index)=> {
+                  filteredData?.slice(page * 6 - 6,page *6).map((product, index)=>{
                     return <ProductCard key={index} product={product} />
-                    
                   })
-
                 }
               </div>
-
             </div>
+             <Pagination pageHandler={pageHandler}  page={page} dynamicPage={dynamicPage}/>
+
+            </>
+            
           ) : (
             <div className='flex items-center justify-center h-[500px] '>
               <video muted autoPlay loop >
