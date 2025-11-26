d52f5d20e9b2b42de357c99cf9e44f3537b147ab
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-11-26 15:14:41 +0600

    login alert

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,24 +1,58 @@
+
+
 import { useUser } from '@clerk/clerk-react';
-import React from 'react';
-import { Navigate } from 'react-router'; // note: react-router-dom
+import React, { useEffect } from 'react';
+import { Navigate } from 'react-router';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ProtectedRoute = ({ children }) => {
   const { user } = useUser();
 
+  useEffect(() => {
+    if (!user) {
+      toast.error("Please login first!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+      });
+    }
+  }, [user]);
+
   if (!user) {
-    
-    alert("Please login first!");
-    return <Navigate to="/" />; 
+    return <Navigate to="/" />;
   }
 
-  // user logged in হলে normal component render হবে
-  return children;
+  return (
+    <>
+      <ToastContainer />
+      {children}
+    </>
+  );
 };
 
 
 
+// import { useUser } from '@clerk/clerk-react';
+// import React from 'react';
+// import { Navigate } from 'react-router'; // note: react-router-dom
 
+// export const ProtectedRoute = ({ children }) => {
+//   const { user } = useUser();
 
+//   if (!user) {
+    
+//     alert("Please login first!" );
+//     return <Navigate to="/" />; 
+//   }
+
+//   // user logged in হলে normal component render হবে
+//   return children;
+// };
 
 
 
